0a7cb4486b0d2f4dea50f5e2e66d2c5e47cc1e82
udalovvv <udalov.vitalii@gmail.com>
2024-01-23 15:04:32 +0500

    Make a user reflector
    Remove imports in User

--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -5,16 +5,22 @@ import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
+import javax.persistence.Id;
+
 @Data
 @NoArgsConstructor
 @RequiredArgsConstructor
 public class User {
 
+    @Id
     private Long id;
+
     @NonNull
     private String name;
+
     @NonNull
     private String lastName;
+
     @NonNull
     private Byte age;
 
